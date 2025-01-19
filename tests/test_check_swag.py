46e2fd9a49f87e963d16d310eff8bb739ef02e0b
Vasiliy_Urazgaliev <Urazgaliev@list.ru>
2025-01-19 22:29:18 +0300

    home_work
    Signed-off-by: Vasiliy_Urazgaliev <Urazgaliev@list.ru>

--- tests/test_check_swag.py
+++ tests/test_check_swag.py
@@ -1,7 +1,7 @@
 from pages.swag_labs import SwagLabs
 
-
-def test_icon(browser):
+# тест кейс, проверка наличия элемента на странице, поля имени и поля пароля
+def test_case(browser):
     swag_page = SwagLabs(browser)
     swag_page.visit()
     assert swag_page.exist_icon()
